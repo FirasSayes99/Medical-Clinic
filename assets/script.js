ae913189e0e9ab2dd4d95a59bb7f1ab07f9ebc13
firassayes <firassayes1999@gmail.com>
2022-06-21 20:13:21 +0300

    add all files
    add all files

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+
+// Get Time 
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+// Get Date
+  const d = new Date();
+  document.getElementById("date-time").innerHTML = d.toDateString();
+/////////////////////////////////////////////////////////////////////////////////////////
